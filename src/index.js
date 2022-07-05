be90c19e20be3b1ff239fd8349ed8e3738998a3a
abdamsab <abdamsabinfo@gmail.com>
2022-07-05 09:39:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ async function startApolloServer(typeDefs, resolvers) {
         trackAPI: new TrackAPI(),
       };
     },
+    context,
+    introspection: true,
   });
 
   const { url, port } = await server.listen({port: process.env.PORT || 4000});
