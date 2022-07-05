244682908d3f3824f41996cdec648d2cc8f48b83
abdamsab <abdamsabinfo@gmail.com>
2022-07-05 09:22:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context,
     dataSources: () => {
       return {
         trackAPI: new TrackAPI(),
